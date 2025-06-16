f19c2bb47ee4b7747942f09be3a953e2a66316d0
Ahmad Faryab Kokab <68786248+afaryab@users.noreply.github.com>
2025-06-16 03:34:49 +0000

    some important commands on entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,12 +4,22 @@
 chown -R www-data:www-data /var/www/storage
 chmod -R 775 /var/www/storage
 
+chown -R www-data:www-data /var/www/bootstrap/cache
+chmod -R 775 /var/www/bootstrap/cache
+
+chown www-data:www-data /var/www/.env
+chmod 775 /var/www/.env
+
+chown www-data:www-data /var/www/database/database.sqlite
+chmod 775 /var/www/database/database.sqlite
+
+
 # important laravel commands
-/usr/local/etc/php /var/www/artisan config:cache
-/usr/local/etc/php /var/www/artisan route:cache
-/usr/local/etc/php /var/www/artisan view:cache
-/usr/local/etc/php /var/www/artisan storage:link
-/usr/local/etc/php /var/www/artisan migrate --force
+php /var/www/artisan config:cache
+php /var/www/artisan route:cache
+php /var/www/artisan view:cache
+php /var/www/artisan storage:link
+php /var/www/artisan migrate --force
 
 
 service nginx start
